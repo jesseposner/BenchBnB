e07f8b2269f6d0941f60f82cb2094acba5dfd30e
Jesse Posner <jesse.posner@gmail.com>
2016-04-25 16:23:43 -0700

    adds react router

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,9 +1,34 @@
-var ReactDOM = require('react-dom'),
-    React = require('react'),
-    Search = require('./components/search');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-document.addEventListener('DOMContentLoaded', function () {
-  ReactDOM.render(
-    <Search />, document.getElementById('content')
-  );
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+var Search = require('./components/Search');
+
+var App = React.createClass({
+  render: function(){
+    return (
+        <div>
+          <header><h1>Bench BnB</h1></header>
+          {this.props.children}
+        </div>
+    );
+  }
+});
+
+var Routes = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Search}/>
+    </Route>
+  </Router>
+);
+
+document.addEventListener('DOMContentLoaded', function(){
+  var root = document.getElementById('content');
+  ReactDOM.render(Routes, root);
 });
